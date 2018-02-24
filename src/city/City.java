0f9ff18c227983e9a5af5b0618d996adf93daede
Quinn's Laptop <Quinn's Laptop@XPS>
2018-02-24 15:37:57 -0500

    sldhdg

--- src/city/City.java
+++ src/city/City.java
@@ -5,11 +5,46 @@ import processing.core.PApplet;
 public class City 
 {
 	private PApplet parent;
-	public int population, farms;
+	public int population, farms, size, houses, geography, jobs;
 	
-	public City()
+	public City(int pop, int farm, int sz, int house, int geo, int job)
 	{
-
+		population = pop;
+		farms = farm;
+		size = sz;
+		houses = house;
+		geography = geo;
+		jobs = job;
+	}
+	
+	public int getJobs()
+	{
+		return jobs;
+	}
+	
+	public int getGeo()
+	{
+		return geography;
+	}
+	
+	public int getHouses()
+	{
+		return houses;
+	}
+	
+	public int getPop()
+	{
+		return population;
+	}
+	
+	public int getFarms()
+	{
+		return farms;
+	}
+	
+	public int getSize()
+	{
+		return size;
 	}
 }
 
