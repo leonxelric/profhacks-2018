65439bda76dcb97501b03629158a28f4b2e2e103
Quinn's Laptop <Quinn's Laptop@XPS>
2018-02-24 17:15:36 -0500

    startup

--- src/city/CityRunner.java
+++ src/city/CityRunner.java
@@ -1,5 +1,32 @@
 package city;
 
-public class CityRunner {
+import processing.core.PApplet;
 
+public class CityRunner extends PApplet
+{
+	int amount = 15;
+	City[] cities = new City[amount];
+			
+	public static void main(String[] args)
+	{
+		PApplet.main("city.City");
+	}
+	
+	public void settings()
+	{
+		size(1800, 900);
+	}
+	
+	public void setup()
+	{
+		for(int i = 0; i < amount; i++)
+		{
+			cities[i] = City(/* put stuff here*/ );
+		}
+	}
+	
+	public void draw()
+	{
+		
+	}
 }
